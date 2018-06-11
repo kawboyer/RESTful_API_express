ba87d89123356f6abb068d99a221b00f254e279d
Klara Boyer <kawboyer@gmail.com>
2018-06-11 15:34:20 -0400

    post request working

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+// Dependencies
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // Connection config
 const mc = mysql.createConnection({
@@ -20,7 +26,7 @@ app.get('/', function(req, res) {
   return res.send({ error: true, message: 'hello' })
 });
 
-// Retrieve all nodes
+// Retrieve nodes
 app.get('/nodes', function (req, res) {
   mc.query('SELECT * FROM nodes', function(error, results, fields) {
     if (error) throw error;
@@ -28,17 +34,43 @@ app.get('/nodes', function (req, res) {
   });
 });
 
+// Retrieve node with id 
+app.get('/node/:id', function(req, res) {
+  let id = req.params.id;
+
+  if (!id) {
+    return res.status(400).send({ error: true, message: 'Please provide id' });
+  }
+  mc.query('SELECT * FROM nodes WHERE id = ?', id, function (error, results, fields) {
+    if (error) throw error;
+    return res.send({ error: false, data: results[0], message: 'Nodes list' });
+  });
+});
+
 // Add a new node
-app.post('/node', function(req, res) {
+app.post('/nodes', function(req, res) {
   let node_name = req.body.node_name;
 
   if(!node_name) {
-    return res.status(400).send({ error: true, message: 'Plesse provide node name' });
+    return res.status(400).send({ error: true, message: 'Please provide node name' });
   }
-
   mc.query('INSERT INTO nodes SET ?', { node_name: node_name }, function (error, results, fields) {
     if (error) throw error;
     return res.send({ error: false, data: results, message: 'New node has been created succesfully!' });
+  });
+});
+
+// Update node
+app.put('/node', function(req, res) {
+  let id = req.body.id;
+  let node_name = req.body.node_name;
+
+  if ( !id || !node_name) {
+    return res.status(400).send({ error: node_name, message: 'Please provide node name and id'});
+  }
+  mc.query('UPDATE nodes SET node_name = ? id = ?', [node_name, id], function(error, results, fields) {
+    if (error) throw error;
+    return res.send({ error: false, data: results, message: 'Task has been updated succesfully' });
   });
 });
 
@@ -55,8 +87,7 @@ app.delete('/node', function(req, res) {
   });
 });
 
-
-// Port set to 8082 for incoming http requests
-app.listen(8082, function() {
-  console.log('Node app is running on port 8082');
+// Port set to 8080 for incoming http requests
+app.listen(8080, function() {
+  console.log('Listening on port 8080');
 });
